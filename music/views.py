dbfdfc7f482a5c5acb0e111cf19beeeaf528d40b
saikatharryc <saikatchakrabortty2@gmail.com>
2017-02-18 11:32:30 +0530

    show data id wise through template engine confirmed

--- music/views.py
+++ music/views.py
@@ -1,18 +1,19 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+
+from django.shortcuts import render
 
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template("music/index.html")
-    context = {
-        'all_albums': all_albums,
-
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details For Album ID:" + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Does Not exist")
+
+    return render(request, 'music/detail.html', {'album': album})
