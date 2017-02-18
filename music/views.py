d9512b620710af03266c7c686aebf02c5ee3a791
saikatharryc <saikatchakrabortty2@gmail.com>
2017-02-18 10:27:09 +0530

    detail view by id confirmed at /music/:id

--- music/views.py
+++ music/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-
     return HttpResponse("<h1>is it??????</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details For Album ID:" + str(album_id) + "</h2>")
